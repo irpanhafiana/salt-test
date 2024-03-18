fe829422de98d5a6c2e9eb882ea0727cd3bc6800
Irpan18 <irpanhafiana@gmail.com>
2024-03-18 08:41:40 +0700

    ganti public api

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,6 @@
 import { Component } from '@angular/core';
-import { News } from './model/news';
 import { NewsService } from './services/news.service';
 import * as moment from 'moment';
-import { Sources } from './model/sources';
 
 @Component({
   selector: 'app-root',
@@ -11,60 +9,53 @@ import { Sources } from './model/sources';
 })
 export class AppComponent {
   title: string = 'salt-testwork';
-  news: News[] = [];
-  newsBitcoin: News[] = [];
-  newsSources: Sources[] = [];
+  data: any[] = [];
+  groupedData: any[][] = [];
   loading: boolean = true;
 
-  constructor(private newsService: NewsService) {}
+  constructor(private service: NewsService) {}
 
   ngOnInit() {
-    this.getTopHeadline();
-    this.getSources();
-    this.getChannels();
+    this.getNews();
   }
 
-  getChannels() {
+  getNews() {
     this.loading = true;
-    this.newsService.getChannel().subscribe(
-      (data: Sources[]) => {
-        this.loading = false;
-        this.newsSources = data;
-        console.log(this.newsSources);
+    this.service.getNews().subscribe({
+      next: (arrId: number[]) => {
+        const per20data = arrId.slice(0, 20);
+        if (per20data) {
+          for (let id of per20data) {
+            this.service.getNewsItem(id).subscribe({
+              next: (detail: any) => {
+                this.loading = false;
+                this.data.push(detail);
+                if (this.data.length === per20data.length) {
+                  this.groupData();
+                }
+              },
+              error: (e) => {
+                this.loading = false;
+                console.log(e);
+              },
+            });
+          }
+        }
       },
-      (error) => {
+      error: (e) => {
         this.loading = false;
-        console.error('Error fetching news:', error);
-      }
-    );
+        console.log(e);
+      },
+    });
   }
 
-  getTopHeadline() {
-    this.loading = true;
-    this.newsService.getNewsTopHeadline().subscribe(
-      (data: News[]) => {
-        this.loading = false;
-        this.news = data;
-      },
-      (error) => {
-        this.loading = false;
-        console.error('Error fetching news:', error);
-      }
-    );
-  }
+  groupData(): void {
+    for (let i = 0; i < this.data.length; i += 5) {
+      this.groupedData.push(this.data.slice(i, i + 5));
+    }
 
-  getSources() {
-    this.loading = true;
-    this.newsService.getNewsSource().subscribe(
-      (data: News[]) => {
-        this.loading = false;
-        this.newsBitcoin = data;
-      },
-      (error) => {
-        this.loading = false;
-        console.error('Error fetching news:', error);
-      }
-    );
+    // console.log(this.groupedData);
+    console.log(this.groupedData);
   }
 
   cutString(desc: string, max: number) {
--- src/app/services/news.service.ts
+++ src/app/services/news.service.ts
@@ -1,47 +1,43 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, map } from 'rxjs';
-import { News } from '../model/news';
-import { Sources } from '../model/sources';
+import { Observable, forkJoin, map, of, switchMap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class NewsService {
-  private apiKey = '807f540d3d334a628346279f94933ae3';
-  private apiUrl = 'https://newsapi.org/v2';
+  private endpoint = 'https://hacker-news.firebaseio.com/v0';
 
   constructor(private http: HttpClient) {}
 
-  getNewsTopHeadline(): Observable<News[]> {
-    return this.http
-      .get<any>(`${this.apiUrl}/top-headlines?country=id&apiKey=${this.apiKey}`)
-      .pipe(
-        map((response: any) => {
-          return response.articles.slice(0, 5);
-        })
-      );
+  getNews(): Observable<number[]> {
+    return this.http.get<number[]>(`${this.endpoint}/topstories.json`);
   }
 
-  getNewsSource(): Observable<News[]> {
-    return this.http
-      .get<any>(`${this.apiUrl}/everything?q=bitcoin&apiKey=${this.apiKey}`)
-      .pipe(
-        map((response: any) => {
-          return response.articles.slice(0, 6);
-        })
-      );
+  getNewsItem(itemId: number): Observable<any> {
+    return this.http.get<any>(`${this.endpoint}/item/${itemId}.json`);
   }
 
-  getChannel(): Observable<Sources[]> {
-    return this.http
-      .get<any>(
-        `${this.apiUrl}/top-headlines/sources?apiKey=${this.apiKey}&pageSize=8`
-      )
-      .pipe(
-        map((response: any) => {
-          return response.sources.slice(0, 8);
+  getNewsItemComments(commentId: number): Observable<any> {
+    return this.http.get<any>(`${this.endpoint}/item/${commentId}.json`);
+  }
+
+  getNestedComments(commentIds: number[]): Observable<any>[] {
+    return commentIds.map((commentId) => {
+      return this.getNewsItemComments(commentId).pipe(
+        switchMap((comment) => {
+          if (comment && comment.kids) {
+            return forkJoin(this.getNestedComments(comment.kids)).pipe(
+              map((nestedComments) => {
+                comment.nestedComments = nestedComments;
+                return comment;
+              })
+            );
+          } else {
+            return of(comment);
+          }
         })
       );
+    });
   }
 }
